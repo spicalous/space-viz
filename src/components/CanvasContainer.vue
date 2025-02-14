9ff19feff62660a9df1f5cdb92e5e25b0660a2be
spicalous <benlertlumprasertkul@gmail.com>
2025-02-14 03:34:27 +0000

    spinning cube

--- src/components/CanvasContainer.vue
+++ src/components/CanvasContainer.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import { useTemplateRef, onMounted } from 'vue';
+import * as THREE from 'three';
+
+const canvas = useTemplateRef('ref-canvas');
+
+const scene = new THREE.Scene();
+const fieldOfViewDegrees = 75;
+const aspectRatio = window.innerWidth / window.innerHeight;
+const nearClippingPane = 0.1;
+const farClippingPane = 1000;
+const camera = new THREE.PerspectiveCamera(fieldOfViewDegrees, aspectRatio, nearClippingPane, farClippingPane);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+onMounted(() => {
+  const renderer = new THREE.WebGLRenderer({ canvas: canvas.value });
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  function animate() {
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+  }
+  renderer.setAnimationLoop( animate );
+});
 </script>
 
 <template>
-  <div class="container">
-    <h1>{{ msg }}</h1>
-  </div>
+  <canvas ref="ref-canvas"></canvas>
 </template>
 
 <style scoped>
--- src/components/__tests__/CanvasContainer.spec.ts
+++ src/components/__tests__/CanvasContainer.spec.ts
@@ -4,8 +4,8 @@ import { mount } from '@vue/test-utils'
 import CanvasContainer from '../CanvasContainer.vue'
 
 describe('CanvasContainer', () => {
-  it('renders properly', () => {
-    const wrapper = mount(CanvasContainer, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+//  it('renders properly', () => {
+//    const wrapper = mount(CanvasContainer, { props: { msg: 'Hello Vitest' } })
+//    expect(wrapper.text()).toContain('Hello Vitest')
+//  })
 })

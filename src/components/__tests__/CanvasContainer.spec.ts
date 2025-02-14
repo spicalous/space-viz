d40b5fd7c2bfe58a2b3091eafd7420936b11cb52
spicalous <benlertlumprasertkul@gmail.com>
2025-02-14 02:40:42 +0000

    empty canvas container

--- src/components/__tests__/CanvasContainer.spec.ts
+++ src/components/__tests__/CanvasContainer.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import CanvasContainer from '../CanvasContainer.vue'
 
-describe('HelloWorld', () => {
+describe('CanvasContainer', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(CanvasContainer, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })

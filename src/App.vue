3d3db53bf09ab8622745c5553b4b12c166894881
spicalous <benlertlumprasertkul@gmail.com>
2025-02-18 19:03:11 +0000

    add compatibility check

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import CanvasContainer from './components/CanvasContainer.vue'
+  import { ref } from 'vue';
+  import WebGL from 'three/addons/capabilities/WebGL.js';
+  import CanvasContainer from './components/CanvasContainer.vue';
+
+  const isWebGL2Available = ref(WebGL.isWebGL2Available());
 </script>
 
 <template>
-  <main>
+  <main v-if="isWebGL2Available">
     <CanvasContainer msg="You did it!" />
+  </main>
+  <main v-else>
+    <p>Looks like your device doesn't support WebGL 2</p>
   </main>
 </template>
 

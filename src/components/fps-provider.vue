5ee3a299c6ef40d445b9f9fee6c83d6eaf975775
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2025-02-24 19:04:08 +0000

    Create fps-provider.vue

---
+++ src/components/fps-provider.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { provide, ref, readonly } from 'vue';
+
+  const fps = ref(0);
+
+  function updateFps(newFps: number) {
+    fps.value = newFps;
+  }
+
+  provide('fps', { fps: readonly(fps), updateFps });
+</script>
